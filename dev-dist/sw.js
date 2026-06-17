f6e479b56a9bc9982c582089fc2aaa95d2031444
collaborate-with-mursaleen <collaborate.with.mursaleen@gmail.com>
2026-06-17 21:43:15 +0530

    manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mu4fecuig3g"
+    "revision": "0.456aimga518"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
