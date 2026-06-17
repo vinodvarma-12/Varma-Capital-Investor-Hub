d262fd3d5318cf5d0e2d61098831ebb27a78a637
collaborate-with-mursaleen <collaborate.with.mursaleen@gmail.com>
2026-06-17 16:14:55 +0000

    test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mu4fecuig3g"
+    "revision": "0.1j1nbplt3ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
