7290878c373c66498e8122d9abcc1976922b7f4c
Mursaleen Nisar <mursaleennisar980@gmail.com>
2026-08-05 11:09:41 +0000

    re-debug onboarding video

--- src/components/investor/PortalOnboarding.jsx
+++ src/components/investor/PortalOnboarding.jsx
@@ -7,15 +7,14 @@ import {
   INVESTOR_ONBOARDING_YOUTUBE_ID,
 } from "@/lib/investorPortal";
 
-useEffect(() => {
-  console.log(INVESTOR_ONBOARDING_YOUTUBE_ID);
-}, []);
-
 export default function PortalOnboarding({
   user,
   onRefresh,
   refreshing = false,
 }) {
+  useEffect(() => {
+    console.log(INVESTOR_ONBOARDING_YOUTUBE_ID);
+  }, []);
   const firstName = user?.full_name?.split(" ")[0] || "Investor";
   const embedUrl = getYouTubeEmbedUrl(INVESTOR_ONBOARDING_YOUTUBE_ID);
 
